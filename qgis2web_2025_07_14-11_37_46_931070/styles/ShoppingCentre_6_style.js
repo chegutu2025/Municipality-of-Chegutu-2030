a3f3a723c4d428ae1e00e787fc295268d238eba7
chegutu2025 <vincentmoyot99@gmail.com>
2025-07-15 16:26:47 +0200

    Add files via upload

---
+++ qgis2web_2025_07_14-11_37_46_931070/styles/ShoppingCentre_6_style.js
@@ -0,0 +1,39 @@
+var size = 0;
+var placement = 'point';
+
+var style_ShoppingCentre_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "#94c094";
+    var bufferWidth = 3.0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("Name") !== null && resolution > 0 && resolution < 4) {
+        labelText = String(feature.get("Name"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [580, 580],
+                  scale: 0.015517241379310345,
+                  anchor: [290.0, 290.0],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/shopping_supermarket.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
